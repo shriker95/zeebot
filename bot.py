db448c51d7d034d49fac9a48908746dfc9c8fd1e
shriker95 <samarcomel@gmail.com>
2025-03-07 23:05:09 +0100

    changed to mistral

--- bot.py
+++ bot.py
@@ -2,44 +2,46 @@
 import os
 from datetime import datetime, timedelta
 from twitchio.ext import commands
-import openai
+from mistralai import Mistral
 from dotenv import load_dotenv
 
 load_dotenv()
 
-
 # Persönlichkeit des Bots
 BOT_PERSONALITY = """
-Du bist Zee. Ein Frau aus Frankfurt. Du lebst vegan und glutenfrei,
+Du bist Zee. Du lebst vegan und glutenfrei,
 liebst Tiere und bist sehr links eingestellt. Du bist ein großer Fan von Elli.
 Du bist gerade Zuschauerin von Elli auf Twitch. Antworte als wärst du Zee.
 """
 
-openai.api_key = os.getenv("OPENAI_KEY")
-openai_model = os.getenv("OPENAI_MODEL")
+mistral_api_key = os.getenv("MISTRAL_KEY")
+mistral_model = os.getenv("MISTRAL_MODEL")
 twitch_token = os.getenv("TWITCH_TOKEN")
 twitch_channel = os.getenv("TWITCH_CHANNEL")
 twitch_client_secret = os.getenv("TWITCH_CLIENT_SECRET")
+timeout = int(os.getenv("TIMEOUT"))
 
-def chat_with_openai(prompt, personality):
-    """Chat with OpenAI's GPT models using the provided prompt and personality."""
+client = Mistral(api_key=mistral_api_key)
+
+def chat_with_mistral(prompt, personality):
+    """Chat with Mistral models using the provided prompt and personality."""
     messages = [
         {"role": "system", "content": personality},
         {"role": "user", "content": prompt}
     ]
-    response = openai.chat.completions.create(
-        model=openai_model,
+    response = client.chat.complete(
+        model=mistral_model,
         messages=messages,
     )
     return response.choices[0].message.content
 
 
 class Bot(commands.Bot):
-    """Twitch bot that uses OpenAI's GPTs to chat with viewers."""
+    """Twitch bot that uses Mistral GPTs to chat with viewers."""
     def __init__(self):
         """Initialize the bot with the Twitch token, channel, and client secret."""
         self.last_used = datetime.min
-        self.cooldown_period = timedelta(seconds=300)
+        self.cooldown_period = timedelta(seconds=timeout)
 
         super().__init__(token=twitch_token, prefix='!',
                          initial_channels=[twitch_channel],
@@ -54,12 +56,12 @@ class Bot(commands.Bot):
         """Handle incoming messages."""
         if message.echo:
             return
-        print(message.content)
+        #print(message.content)
         await self.handle_commands(message)
 
     @commands.command()
     async def zee(self, ctx: commands.Context):
-        """Chat as Zee using OpenAI's GPT."""
+        """Chat as Zee using Mistral GPT."""
         now = datetime.now()
 
         # Check if the command is on cooldown
@@ -74,10 +76,10 @@ class Bot(commands.Bot):
         # Update the last used time
         self.last_used = now
 
-        print(ctx.message.content)
+        #print(ctx.message.content)
         prompt = ctx.message.content[len('!zee'):].strip()
-        print(prompt)
-        response = chat_with_openai(prompt, BOT_PERSONALITY)
+        #print(prompt)
+        response = chat_with_mistral(prompt, BOT_PERSONALITY)
         await ctx.send(response[:500])
 
 
